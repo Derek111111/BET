3400d2d575d7e4f268071f9fe150720603d76eb4
Minal Karani <karani.minal@gmail.com>
2019-03-05 19:40:18 -0500

    MVC2

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,29 @@
-CREATE DATABASE bet_db;
-USE bet_db;
+DROP DATABASE bet;
+
+CREATE DATABASE bet;
+USE bet;
 
 CREATE TABLE users
 (
-	id int NOT NULL AUTO_INCREMENT,
-	user_name varchar(255) NOT NULL,
-	email_id varchar(255) NOT NULL,
-	password varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    id int NOT NULL AUTO_INCREMENT,
+    user_name varchar(255) NOT NULL,
+    email_id varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 CREATE TABLE category
 (
-	id integer NOT NULL AUTO_INCREMENT,
-	category_name varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    id integer NOT NULL AUTO_INCREMENT,
+    category_name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 CREATE TABLE bills
 (
-	id integer NOT NULL AUTO_INCREMENT,
-	amount integer(100) NOT NULL,
-    bill_date timestamp,
-	category varchar(255) NOT NULL,
-	spent_at integer(100) NOT NULL,
-	remarks varchar(255) NOT NULL,
-	PRIMARY KEY (id)
+    id integer NOT NULL AUTO_INCREMENT,
+    amount Decimal (10,2) NOT NULL,
+   bill_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    category varchar(255) NOT NULL,
+    spent_at varchar(100) NOT NULL,
+    remarks varchar(255) ,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO users (user_name,email_id,password) VALUES ("John","john@abc.com","Year2019");
+INSERT INTO category (category_name) VALUES ("Gas");
+
+INSERT INTO bills (amount, category, spent_at,remarks) VALUES (3.50, "Gas", "costco", "Gas Bill");
