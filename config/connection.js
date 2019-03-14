5abc6c37490a4d769711b5731e7e2a8e96907616
Minal Karani <karani.minal@gmail.com>
2019-03-13 22:00:49 -0400

    Final

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,21 @@
-// Set up MySQL connection.
+
 var mysql = require("mysql");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "y54zw1dxwm24yri8",
-  password: "eqe98wz92ng8uhz7",
-  database: "a1vmxm3ork5u6cdd"
-  
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: "Min143Sag",
+        database: 'bet_db'
+    })
+};
+connection.connect();
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
 module.exports = connection;
