23015e0c3b8ea98f31adb0383f3c178723b7b335
Minal Karani <karani.minal@gmail.com>
2019-03-14 09:04:54 -0400

    Final BET

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
-
+// Set up MySQL connection.
 var mysql = require("mysql");
-require("dotenv").config();
 
-var connection;
-if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // Database is local
-    connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: 'root',
-        password: "Min143Sag",
-        database: 'bet_db'
-    })
-};
-connection.connect();
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Min143Sag",
+  database: "bet_db"
+});
 
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
